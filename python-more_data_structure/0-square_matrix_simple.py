158e4634c2a777eeeb2d3988af58d0d061a54e63
Silvycr <silvia_alinacr@live.com>
2023-07-17 13:56:27 -0700

    Squared simple

--- python-more_data_structure/0-square_matrix_simple.py
+++ python-more_data_structure/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    novo_matrix = []
-    for y in matrix:
-        novo_matrix.append(list(map(lambda x: x ** 2, y)))
-    return novo_matrix
+    new_matrix = []
+    for a in matrix:
+        new_matrix.append(list(map(lambda x: x ** 2, a)))
+    return new_matrix
