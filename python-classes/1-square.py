c8014ae414d86a77df21929266ce7074e2ea2144
Silvycr <silvia_alinacr@live.com>
2023-07-22 16:51:03 -0700

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-""" defines a square """
+""" defines a square based on size  """
+
+
 class Square:
-     """ square with attribute size """
+    """ square with attribute size """
+
     def __init__(self, size):
+
+        """
+        Args:
+            size: size of square
+        """
+
         self.__size = size
