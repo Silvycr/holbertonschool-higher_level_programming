631e1b9b79b4bebf29af097b0ce5f7fa63c56d5c
Silvycr <silvia_alinacr@live.com>
2023-07-29 10:35:14 -0700

    Full rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" contains subclass Rectangle which 
+""" contains subclass Rectangle which
 inherits from class BaseGeometry """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
