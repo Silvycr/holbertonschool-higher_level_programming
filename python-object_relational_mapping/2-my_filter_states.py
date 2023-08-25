af2b0ff07f5eb74fd5b22ee3299506ae6aff2c49
Silvycr <silvia_alinacr@live.com>
2023-08-24 18:30:26 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa."""
 
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
