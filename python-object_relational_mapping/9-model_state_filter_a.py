3f934296496c3f33d548dd5f60428190e3419869
Silvycr <silvia_alinacr@live.com>
2023-08-25 16:37:03 -0700

    Contains

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,3 +18,6 @@ if __name__ == '__main__':
     for state in session.query(State).order_by(State.id):
         if 'a' in state.name:
             print('{}: {}'.format(state.id, state.name))
+    
+    session.commit()
+    session.close()
