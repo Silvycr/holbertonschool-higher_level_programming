2da1964b560130ada0af361468e11f769b871755
Silvycr <silvia_alinacr@live.com>
2023-08-24 18:39:59 -0700

    SQL Injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,8 +7,12 @@ import sys
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd='', db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd='',
+                         db=sys.argv[3],
+                         charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` WHERE `name`=%s\
                 ORDER BY `id` ASC", (sys.argv[4],))
