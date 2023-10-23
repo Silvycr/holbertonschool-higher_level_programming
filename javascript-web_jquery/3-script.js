635402a959bf13bbe0a411911aa041feed0a87b9
Silvycr <silvia_alinacr@live.com>
2023-10-22 21:49:05 -0700

    Add  class

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+/*
+  Updates the text color
+  to #FF0000 with .red class
+*/
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('header').addClass('red');
+  });
+});
