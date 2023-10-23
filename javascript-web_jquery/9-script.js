41fe332d266032650e66fd810841163ede91a244
Silvycr <silvia_alinacr@live.com>
2023-10-22 21:52:05 -0700

    Say Hello!

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+/*
+  Write a Javascript script that fetches from 
+  https://fourtonfish.com/hellosalut/?lang=fr and 
+  displays the value of hello from that fetch in the HTML’s tag DIV#hello.
+*/
+$(document).ready(function () {
+  $.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (result) {
+      $('div#hello').text(result.hello);
+    });
+});
