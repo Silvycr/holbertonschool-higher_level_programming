5fa831b57103d0ecf326ef3bfcde2ba614278921
Silvycr <silvia_alinacr@live.com>
2023-10-22 21:50:53 -0700

    Star wars character

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+/*
+  Fetches and replaces the
+  name of this URL: https://swapi.co/api/people/5/?format=json
+*/
+$(document).ready(function () {
+    $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) {
+        $('div#character').text(result.name);
+      });
+    });
